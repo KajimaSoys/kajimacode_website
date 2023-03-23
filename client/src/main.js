cc83fb25184520600bc36b3e1ac55339c987f028
Grishin Vadim <43302420+KajimaSoys@users.noreply.github.com>
2023-03-23 17:25:52 +0400

    v.2.8.3 - Favicon update

--- client/src/main.js
+++ client/src/main.js
@@ -55,5 +55,5 @@ app.config.globalProperties.$hostname = 'https://kajimacode.com'
 // prod
 axios.defaults.baseURL = 'https://kajimacode.com'
 
-app.config.globalProperties.$projectVersion = '2.8.2'
+app.config.globalProperties.$projectVersion = '2.8.3'
 
