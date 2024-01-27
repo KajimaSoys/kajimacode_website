e97a2aa9017e8e54bf375e9bb08828c899269d42
Grishin Vadim <43302420+KajimaSoys@users.noreply.github.com>
2024-01-27 05:12:09 +0400

    v.4.0.0 - Docker wrap

--- kajimasoys_portfolio/prod_settings.py
+++ kajimasoys_portfolio/prod_settings.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = (os.environ.get('KAJIMASOYS_PORTFOLIO_DEBUG') == 'True')
-ALLOWED_HOSTS = os.environ.get('KAJIMASOYS_PORTFOLIO_ALLOWED_HOSTS', '127.0.0.1').split(',')
-SECRET_KEY = os.environ.get('KAJIMASOYS_PORTFOLIO_SECRET_KEY')
+DEBUG = (os.environ.get('DEBUG') == 'True')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",# from nginx in prod
@@ -13,22 +13,22 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('KAJIMASOYS_PORTFOLIO_POSTGRES_NAME'),
-        'USER': os.environ.get('KAJIMASOYS_PORTFOLIO_POSTGRES_USER'),
-        'PASSWORD': os.environ.get('KAJIMASOYS_PORTFOLIO_POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('KAJIMASOYS_PORTFOLIO_POSTGRES_HOST'),
-        'PORT': os.environ.get('KAJIMASOYS_PORTFOLIO_POSTGRES_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-CSRF_COOKIE_SECURE = (os.environ.get('KAJIMASOYS_PORTFOLIO_CSRF_COOKIE_SECURE', False) == 'True')
-SESSION_COOKIE_SECURE = (os.environ.get('KAJIMASOYS_PORTFOLIO_SESSION_COOKIE_SECURE', False) == 'True')
+CSRF_COOKIE_SECURE = (os.environ.get('CSRF_COOKIE_SECURE', False) == 'True')
+SESSION_COOKIE_SECURE = (os.environ.get('SESSION_COOKIE_SECURE', False) == 'True')
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/home/kajimasoys/kajimasoys_portfolio/kajimasoys_portfolio/cache'
+        'LOCATION': '/app/kajimasoys_portfolio/cache'
     }
 }
